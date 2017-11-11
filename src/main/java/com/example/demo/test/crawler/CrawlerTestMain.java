874bc982b1ddb3750682e65eafe534ec484670b6
writer <rainman110@163.com>
2017-11-11 19:28:22 +0800

    modify crawler test

--- src/main/java/com/example/demo/test/crawler/CrawlerTestMain.java
+++ src/main/java/com/example/demo/test/crawler/CrawlerTestMain.java
@@ -1,8 +1,13 @@
 package com.example.demo.test.crawler;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
@@ -13,13 +18,15 @@ import org.apache.http.ssl.SSLContextBuilder;
 import javax.net.ssl.SSLContext;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CrawlerTestMain {
 
 
     public static void main(String[] args){
 
-        WebClient webClient = new WebClient();
+        WebClient webClient = new WebClient(BrowserVersion.CHROME);
         webClient.getOptions().setCssEnabled(false);
         webClient.getOptions().setJavaScriptEnabled(true);
         webClient.getOptions().setThrowExceptionOnScriptError(false);
@@ -28,13 +35,35 @@ public class CrawlerTestMain {
         try {
             final HtmlPage htmlPage = webClient.getPage("http://www.iqiyi.com");
 
+            Thread.sleep(2 * 1000);
             // TODO check anchor href. let illegal to be legal
             HtmlAnchor dianYingAnchor = htmlPage.getAnchorByText("电影");
             HtmlPage dianYingPage = webClient.getPage("http:" + dianYingAnchor.getHrefAttribute());
 
+            Thread.sleep(2 * 1000);
             HtmlAnchor freeMovieAnchor = dianYingPage.getAnchorByText("免费电影");
             HtmlPage freeMoviePage = webClient.getPage(freeMovieAnchor.getHrefAttribute());
 
+            // 获取文档中所有的div
+            List<HtmlDivision> htmlDivisions = (List<HtmlDivision>) freeMoviePage.getByXPath("//div");
+
+            // 通过css选择器Selector获取元素
+            // 获取当前页所有class="site-piclist_pic"的元素 返回DOM节点列表
+            DomNodeList<DomNode> divDomNodes = freeMoviePage.querySelectorAll(".site-piclist_pic");
+            List<Movie> movies = new ArrayList<>(divDomNodes.size());
+
+            for (DomNode domNode : divDomNodes) {
+                HtmlAnchor a = domNode.querySelector("a");
+                String href = a.getAttribute("href").trim();
+                String title = a.getAttribute("title").trim();
+
+                Movie tmpMovie = new Movie();
+                tmpMovie.setTitle(title);
+                tmpMovie.setUrl(href);
+                tmpMovie.setFrom("www.iqiyi.com");
+
+                movies.add(tmpMovie);
+            }
 
             System.out.println("爱奇艺首页 title: " + htmlPage.getTitleText());
 
@@ -44,6 +73,12 @@ public class CrawlerTestMain {
             System.out.println("free电影 href: " + freeMovieAnchor.getHrefAttribute());
             System.out.println("free电影 title: " + freeMoviePage.getTitleText());
 
+            for (Movie movie : movies) {
+                System.out.println(movie);
+            }
+
+            Thread.sleep(4 * 1000);
+            webClient.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -68,6 +103,22 @@ public class CrawlerTestMain {
                 setSSLSocketFactory(sslsf)//
                 // .setProxy(new HttpHost("127.0.0.1", 8888))
                 .build();
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @ToString
+    static class Movie {
+        private String title; // 片名
+        private String url; // 链接
+
+        private String score; // 评分
+        private int time; // 时长
+        private String from; // 来源
+        private String info; // 视频介绍
+        private String tag; // 视频标签
+
     }
 
 }
